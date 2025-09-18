d3d763b4bed43dcc25b15d6f7c7d985f59bb66cc
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-09-18 23:09:39 +0530

    add student profile page

---
+++ src/student/studentProfilePage/profile.js
@@ -0,0 +1,9 @@
+// Profile page interactivity
+
+document.querySelector('.upload-btn').addEventListener('click', () => {
+  alert('Profile picture upload functionality to be implemented.');
+});
+
+document.querySelector('.edit-btn').addEventListener('click', () => {
+  alert('Edit profile functionality to be implemented.');
+});
